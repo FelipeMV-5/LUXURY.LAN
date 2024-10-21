ace841bd8cb81dc635e5abbd3ab93538a4c47cbb
FelipeMV-5 <fmcg8213@gmail.com>
2024-10-21 17:45:46 +0000

    El felipe tiene alopecia

---
+++ contacto.cs
@@ -0,0 +1,42 @@
+#CSS
+
+body{
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f4;
+    padding: 20px:   
+}
+
+.contact-form{
+    background-color: white;
+    padding: 20px;
+    border-radius: 5px;
+    max-width: 600px;
+    margin: auto;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+.contact-form h2 {
+    margin-bottom: 20px;
+}
+.contact-form input, .contact-form textarea{
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 10px; 
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
+.contact-form button {
+    background-color: #28A745;
+    color: white;
+    border: none;
+    padding: 10xp 20px;
+    border-radius: 4px;
+    cursor: pointer;
+}
+.contact-form button:hover {
+    background-color: #218838;
+}
+#formFeedback {
+    margin-top: 20px;
+    font-weight: bold;
+    color: red;
+}
